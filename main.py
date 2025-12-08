479048899723ff398ee0dd595b98e5030eb59eb7
Varun D Soni <pe02-lt-065@petplpress.com>
2025-12-08 14:12:19 +0530

    made layer logic

--- main.py
+++ main.py
@@ -1,15 +1,51 @@
 import numpy as np
+np.random.seed(0)
 
-inputs =[1,2,3,2.5]
-weights =[[0.2,0.8,-0.5,1.0],
-          [0.5,-0.91,0.26,-0.5],
-          [-0.26,-0.27,0.17,0.87]]
-biases = [2,3,0.5]
+X =[[1,2,3,2.5],
+         [2.0,5.0,-1.0,2.0],
+         [-1.5,2.7,3.3,-0.8]]
+
+class Layer_Dense:
+    def __init__(self,n_inputs,n_neurons):
+        self.weights =0.1*np.random.randn(n_inputs,n_neurons)
+        self.biases=np.zeros((1,n_neurons))
+        pass
+    def forward(self,inputs):
+        self.output =np.dot(inputs,self.weights)+self.biases
+        pass
+
+layer1 =Layer_Dense(4,5)
+layer2=Layer_Dense(5,5)
 
 
-output=np.dot(weights,inputs) +biases
-print(output)
-#   output = input*weights + bais
+layer1.forward(X)
+print(layer1.output)
+
+layer2.forward(layer1.output)
+print(layer2.output)
+# weights =[[0.2,0.8,-0.5,1.0],
+#           [0.5,-0.91,0.26,-0.5],
+#           [-0.26,-0.27,0.17,0.87]]
+# biases = [2,3,0.5]
+
+# weights2 =[[0.1,-0.4,0.5],
+#           [0.5,0.12,0.33],
+#           [-0.44,0.73,-0.13]]
+# biases2 = [-1,2,-0.5]
+
+# #   output = input*weights + bais
+# layer1_outputs=np.dot(inputs,np.array(weights).T) +biases
+# layer2_outputs=np.dot(layer1_outputs,np.array(weights2).T) +biases2
+
+
+# print(layer2_outputs)
+
+
+
+
+
+
+
 '''layer_outputs=[]
 for neuron_weights,neuron_bias in zip(weights,biases):
     neuron_output=0
